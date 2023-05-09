cf69cde48e3d0b0f3105c229dac7d0f015f0c39c
pablo <pablo.la@live.com>
2023-05-09 21:03:31 +0200

    weapons ready

--- code/level.py
+++ code/level.py
@@ -5,6 +5,7 @@ from player import Player
 from support import *
 from debug import debug
 from random import choice
+from weapon import Weapon
 
 
 # Contiene los elementos que va a tener el juego y sus interacciones
@@ -18,6 +19,9 @@ class Level:
         # sprite group setup
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
+
+        # attack sprites
+        self.current_attack = None
 
         # sprite setup
         self.create_map()
@@ -49,7 +53,18 @@ class Level:
                             surf = graphics['objects'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
 
-        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites,self.create_attack,self.destroy_attack)
+
+    def create_attack(self):
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
+
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
+
+
+
 
     def run(self):
         # update and draw the game
