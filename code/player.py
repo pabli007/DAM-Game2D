bbeca67458feb0f94b16d485700a8f2b2994bde5
pablo <pablo.la@live.com>
2023-05-22 19:18:11 +0200

    fixes2.0

--- code/player.py
+++ code/player.py
@@ -9,7 +9,7 @@ class Player(Entity):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
-        self.hitbox = self.rect.inflate(0, -26)
+        self.hitbox = self.rect.inflate(-6, HITBOX_OFFSET['player'])
 
         # graphics setup
         self.import_player_assets()
