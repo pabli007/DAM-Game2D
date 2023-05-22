3f123731005367850b880672f4631dbc3bc4f87c
pablo <pablo.la@live.com>
2023-05-22 19:11:27 +0200

    menu ready

--- code/upgrade.py
+++ code/upgrade.py
@@ -5,6 +5,7 @@ from settings import *
 class Upgrade:
     def __init__(self, player):
         # general setupp
+        self.item_list = None
         self.display_surface = pygame.display.get_surface()
         self.player = player
         self.attribute_nr = len(player.stats)
